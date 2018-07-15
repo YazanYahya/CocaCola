07da6c1110540c8bfa2d5ac512caaa921416642c
YazanYahya <1140010@student.birzeit.edu>
2018-07-15 09:34:14 +0300

    changes

--- src/main/java/edu/birzeit/cocacola/application/controller/UserController.java
+++ src/main/java/edu/birzeit/cocacola/application/controller/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
             return new ResponseEntity<>(user, HttpStatus.OK);
         else
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-c
+
     }
 
 
